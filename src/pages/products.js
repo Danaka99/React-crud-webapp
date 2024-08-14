2e1d89c283a9780f1a8baa40dc6ad8f0b01525e2
Danaka99 <140838401+Danaka99@users.noreply.github.com>
2024-08-14 22:50:59 +0530

    modify

--- src/pages/products.js
+++ src/pages/products.js
@@ -46,6 +46,39 @@ function ProductList(props) {
             <button onClick={props.showForm} type="button" className="btn btn-primary me-2">
                 Create
             </button>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th> Id </th>
+                        <th> Name </th>
+                        <th> Brand </th>
+                        <th> Category </th>
+                        <th> Price </th>
+                        <th> Created At </th>
+                        <th> Action </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map((product, index) => {
+                            return(
+                                <tr key={index}>
+                                    <td>{product.id}</td>
+                                    <td>{product.name}</td>
+                                    <td>{product.brand}</td>
+                                    <td>{product.category}</td>
+                                    <td>{product.price}</td>
+                                    <td>{product.createdAt }</td>
+                                    <td style={{width: "10px", whiteSpace: "nowrap"}}>
+                                        <button type="button" className="btn btn-primary btn-sm me-2">Edit</button>
+                                        <button type="button" className="btn btn-primary btn-sm">Delete</button>
+                                    </td> 
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
         </>
     );
 }
